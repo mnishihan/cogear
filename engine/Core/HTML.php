5d343a6205d08c814f9c634b7a808ed34fe13123
romartyn <romartyn@gmail.com>
2012-01-21 00:34:28 +0400

    Добавил public static function check_plain

--- engine/Core/HTML.php
+++ engine/Core/HTML.php
@@ -42,6 +42,19 @@ final class HTML {
     }
     
     /**
+	 * Encode special characters in a plain-text string for display as HTML
+	 *
+	 * @param string $text
+	 *			The text to be checked or processed.
+	 * @return 
+	 *			An HTML safe version of $text
+	 */
+	
+	public static function check_plain($text) {
+		return htmlspecialchars($text, ENT_QUOTES);
+	}
+    
+    /**
      * Encode special characters in a plain-text string for display as HTML
 	 *
 	 * @param string $text
