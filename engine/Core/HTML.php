cc8cb80140948632fac99db5119a648671a14af7
CuamckuyKot <CuamckuyKot@192.168.1.103>
2012-02-12 09:39:09 +0300

    Delete doubled function — check_plain().

--- engine/Core/HTML.php
+++ engine/Core/HTML.php
@@ -53,19 +53,6 @@ final class HTML {
 	public static function check_plain($text) {
 		return htmlspecialchars($text, ENT_QUOTES);
 	}
-    
-    /**
-     * Encode special characters in a plain-text string for display as HTML
-	 *
-	 * @param string $text
-	 *			The text to be checked or processed.
-	 * @return 
-	 *			An HTML safe version of $text
-	 */
-	
-	public static function check_plain($text) {
-		return htmlspecialchars($text, ENT_QUOTES);
-	}
 
     /**
      * Filter attributes for defined tag
